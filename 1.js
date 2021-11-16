18001493b2cf4851cc0eb2a4a783b988c39cf8a5
alkindi az <37734368+alkindiaz@users.noreply.github.com>
2021-11-16 10:44:31 +0700

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,69 @@
+/*
+Hitung harga total beli Telur
+
+Kamu diminta untuk membuat sebuah program untuk menghitung total harga yang harus dibayar kan oleh customer.
+Customer dipresentasikan memiliki variabel customer, variabel jumlah beli (satuan kilogram), dan variabel member.
+Harga telur per kilo adalah 20000.
+Step pengecekannya adalah :
+
+    1. Jika pengunjung tidak ada, maka program akan mengeluarkan ouput "Hari ini tidak ada customer." dan program akan berhenti. Jika ada lanjut ke step - 2.
+    
+    2. Toko ini juga memiliki potongan harga, tergantung dari banyak jumlah yang dibeli oleh customer. yaitu :
+        - Jika customer membeli telur dengan jumlah 1kg sampai 1000kg, maka tidak mendapat potongan harga.
+        - Jika customer membeli telur dengan jumlah 1001kg sampai 2000kg, maka akan mendapat potongan harga sebesar 10%.
+        - Jika customer membeli telur dengan jumlah lebih besar dari 2000kg, maka akan mendapat potongan harga sebesar 20%.
+    
+    3. Dan customer akan mendapatkan diskon tambahan jika status membernya benar, yaitu 10% dari harga total.
+
+    4. Jika semua proses sudah selesai, maka ouput yang harus dikeluarkan adalah :
+        "[customer] membeli [jumlah Beli] Kg telur, dengan harus membayar Rp.[harga Total]."
+
+Contoh : 
+    1.  input :
+            var customer = "Alleria"
+            var jumlahBeli = 2000
+            var member = true
+        ouput :
+            "Alleria membeli 2000 Kg telur, dengan harus membayar Rp.32400000."
+
+    2.  input :
+            var customer = "Xin"
+            var jumlahBeli = 2000
+            var member = false
+        ouput :
+            "Xin membeli 2000 Kg telur, dengan harus membayar Rp.36000000."
+
+    3.  input :
+            var customer = "Leoric"
+            var jumlahBeli = 1000
+            var member = true
+        ouput :
+            "Leoric membeli 1000 Kg telur, dengan harus membayar Rp.18000000."
+
+    4.  input :
+            var customer = "Eredar"
+            var jumlahBeli = 1000
+            var member = false
+        ouput :
+            "Eredar membeli 1000 Kg telur, dengan harus membayar Rp.20000000."
+
+    5.  input :
+            var customer = ""
+            var jumlahBei = 1000
+            var member = true
+        ouput :
+            "Hari ini tidak ada customer."
+
+Buatlah Pseudocode nya!
+
+NOTES :
+1. Tidak boleh menggunakan built-in function apapun.
+2. Kalian hanya wajib membuat pseudocodenya saja.
+3. Penulisan pseudocode yang tidak rapi akan berpengaruh terhadap nilai
+*/ 
+
+/*
+TULIS PSEUDOCODE KALIAN DISINI
+==============================
+write here...
+*/
